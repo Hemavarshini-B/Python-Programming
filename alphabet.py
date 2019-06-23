a52b2abea949bb1ce99559760a007823240e4f27
Hemavarshini-B <51220049+Hemavarshini-B@users.noreply.github.com>
2019-06-23 19:12:25 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+li=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ab=int(input())
+if ab in li:
+  print("Alphabet")
+else:
+  print("No")
