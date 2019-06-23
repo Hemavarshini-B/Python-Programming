36b69ad1fbaf3ed9ff816c73290804f991212819
Hemavarshini-B <51220049+Hemavarshini-B@users.noreply.github.com>
2019-06-23 19:28:37 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,5 @@
-li=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-ab=int(input())
-if ab in li:
+ab=input()
+if ab.isalpha():
   print("Alphabet")
 else:
   print("No")
