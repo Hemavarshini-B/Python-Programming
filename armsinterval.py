2ef7f9a1f2e878cee68c2c2cbfced1c106e736b8
Hemavarshini-B <51220049+Hemavarshini-B@users.noreply.github.com>
2019-07-04 07:22:01 +0530

    Update armsinterval.py

--- armsinterval.py
+++ armsinterval.py
@@ -1,11 +1,10 @@
 a,b=map(int,input().split())
 for i in range(a,b+1):
-  ord=len(str(i))
   sum=0
   tmp=i
   while tmp>0:
     digit=tmp%10
-    sum=sum+digit**ord
+    sum=sum+digit**3
     tmp=tmp//10
   if i==sum:
-    print(i)
+    print(i,end=' ')
